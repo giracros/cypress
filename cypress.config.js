2348639e2a8d920a971bbf26b9a4eabe9779a151
Daniel Fuentes <77737204+danihourly5@users.noreply.github.com>
2023-01-25 08:35:11 -0500

    initial 1.0

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'gm9jf8',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
